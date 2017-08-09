92b692ea2d22d77343e0337705cdd1332e995ffb
Jiang-To <devtonyjiang@gmail.com>
2017-08-10 01:56:01 +0800

    add project basic files

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+        <div class="ui centered card" v-for="todo in todos" :key="todo.id">
+            <div class="content">
+                <div class="header">
+                    {{ todo.title }}
+                </div>
+                <div class="meta">
+                    {{ todo.project }}
+                </div>
+                <div class="extra content">
+                    <span class="right floated edit icon">
+                        <i class="edit icon"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['todos']
+}
+</script>
+
+
